62f82bf2b15b4779ff3863dfa03f1548da217b1a
phatphoom <neilbunner@gmail.com>
2026-07-26 02:15:27 +0700

    refactor:update environment variable names

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const conn = mysql.createPool({
-  host: process.env.host,
-  user: process.env.user,
-  password: process.env.password,
-  database: process.env.dbName,
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
 });
 
 module.exports = conn;
